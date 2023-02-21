c4d1dce16cca950300eca097448c2b8f2b76b7c2
ADMIN <thanhnhanlun200823@gmail.com>
2023-02-21 17:24:13 +0700

    update code

--- TechnologyAccessories/database/migrations/2023_02_13_033623_create_orders_table.php
+++ TechnologyAccessories/database/migrations/2023_02_13_033623_create_orders_table.php
@@ -13,13 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('total');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
         });
     }
-
     /**
      * Reverse the migrations.
      *
@@ -27,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('orders');
     }
 };
